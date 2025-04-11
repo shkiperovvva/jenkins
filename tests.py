78a18c114604afb9b8b63b9be3c82a6c06a45939
shkiperovvva <167472037+shkiperovvva@users.noreply.github.com>
2025-04-11 11:15:47 +0300

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,29 @@
+import unittest
+from math_operations import add, subtract, multiply, divide # type: ignore
+
+class TestMathOperations(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(add(10, 5), 15)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(-1, -1), -2)
+
+    def test_subtract(self):
+        self.assertEqual(subtract(10, 5), 5)
+        self.assertEqual(subtract(-1, 1), -2)
+        self.assertEqual(subtract(-1, -1), 0)
+
+    def test_multiply(self):
+        self.assertEqual(multiply(10, 5), 50)
+        self.assertEqual(multiply(-1, 1), -1)
+        self.assertEqual(multiply(-1, -1), 1)
+
+    def test_divide(self):
+        self.assertEqual(divide(10, 2), 5)
+        self.assertEqual(divide(-1, 1), -1)
+        self.assertEqual(divide(-1, -1), 1)
+        with self.assertRaises(ZeroDivisionError):
+            divide(10, 0)
+
+if __name__ == '__main__':
+    unittest.main()
